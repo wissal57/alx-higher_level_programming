522c4b01556dc20098f12ac0037e6145068b96a3
wissal57 <ahajamwissal@gmail.com>
2024-01-04 11:55:01 -0800

    python2

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,6 @@ from magic_calculation_102 import add, sub
 
 
 def magic_calculation(a, b):
-   add, sub = magic_calculation_102.add, magic_calculation_102.sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
