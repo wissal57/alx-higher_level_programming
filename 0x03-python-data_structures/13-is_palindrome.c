c25bbe824bc9f96b48265a757dc8df137375fbd4
wissal57 <ahajamwissal@gmail.com>
2024-01-06 05:25:21 -0800

    python3

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,19 +8,19 @@
  */
 void reverse_listint(listint_t **head)
 {
-  listint_t *prev = NULL;
-  listint_t *current = *head;
-  listint_t *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-  while (current)
-    {
-      next = current->next;
-      current->next = prev;
-      prev = current;
-      current = next;
-    }
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-  *head = prev;
+	*head = prev;
 }
 
 /**
@@ -31,43 +31,42 @@ void reverse_listint(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-  if (*head == NULL || (*head)->next == NULL)
-    return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-  while (1)
-    {
-      fast = fast->next->next;
-      if (!fast)
+	while (1)
 	{
-	  dup = slow->next;
-	  break;
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-      if (!fast->next)
-	{
-	  dup = slow->next->next;
-	  break;
-	}
-      slow = slow->next;
-    }
 
-  reverse_listint(&dup);
+	reverse_listint(&dup);
 
-  while (dup && temp)
-    {
-      if (temp->n == dup->n)
+	while (dup && temp)
 	{
-	  dup = dup->next;
-	  temp = temp->next;
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
 	}
-      else
+
+	if (!dup)
+		return (1);
+
 	return (0);
-    }
-
-  if (!dup)
-    return (1);
-
-  return (0);
 }
-
